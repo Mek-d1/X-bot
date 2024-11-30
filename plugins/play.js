4d0169c8f826eabd7fec16bdd5fd883fdc536baa
Mek-d1 <davidpromise2003@gmail.com>
2024-11-30 10:52:31 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -18,6 +18,7 @@ const axios = require("axios");
 const fetch = require("node-fetch");
 const path = require("path");
 var videotime = 2000;
+const gifted = require("gifted-dls");
 const { cmd } = require("../lib/plugins");
 
 smd({
@@ -50,6 +51,10 @@ smd({
 
     // Use the new API to get download links
     const downloadApiUrl = "https://api.giftedtech.my.id/api/download/ytaudio?apikey=gifted&url=" + encodeURIComponent(_0x4f86cb.url);
+    const sampleUrl = 'https://youtu.be/wJnBTPUQS5A?si=klI0qxyOnnbcriOk'; 
+    let data = await gifted.giftedyta(sampleUrl)
+    console.log(data);
+    
     
     let _0x4acf6c = 3; // Retry logic
     while (_0x4acf6c > 0) {
